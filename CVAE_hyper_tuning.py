9832156cf0e7105e42f1b0f41062ee034d842ef6
williamtraynor <2381137t@student.gla.ac.uk>
2023-02-07 15:59:54 +0000

    CVAE model implementation and tuning files

---
+++ CVAE_hyper_tuning.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2022/7/15
+# @Author : Gaowei Zhang
+# @Email  : zgw15630559577@163.com
+import os
+import unittest
+from recbole.trainer import HyperTuning
+from recbole.config import Config
+from recbole.data import (
+    create_dataset,
+    data_preparation,
+)
+from recbole.utils import (
+    get_model,
+    get_trainer,
+    init_seed,
+)
+
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+config_file_list = [os.path.join(current_path, "hyper_tuning/CVAE/CVAE_tuning.yaml")]
+params_file = os.path.join(current_path, "hyper_tuning/CVAE/CVAE_hyper_tuning_params.yaml")
+output_file = os.path.join(current_path, "hyper_tuning/CVAE/CVAE.txt")
+
+
+def objective_function(config_dict=None, config_file_list=None):
+
+    config = Config(config_dict=config_dict, config_file_list=config_file_list)
+    init_seed(config['seed'], False)
+    dataset = create_dataset(config)
+    train_data, valid_data, test_data = data_preparation(config, dataset)
+    model_name = config['model']
+    model = get_model(model_name)(config, train_data._dataset).to(config['device'])
+    trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
+    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data, verbose=False)
+    test_result = trainer.evaluate(test_data)
+
+    return {
+        'model': model_name,
+        'best_valid_score': best_valid_score,
+        'valid_score_bigger': config['valid_metric_bigger'],
+        'best_valid_result': best_valid_result,
+        'test_result': test_result
+    }
+
+
+def tune(algo):
+    hp = HyperTuning(
+        objective_function,
+        algo=algo,
+        early_stop=10,
+        max_evals=75,
+        params_file=params_file,
+        fixed_config_file_list=config_file_list,
+    )
+    hp.run()
+    hp.export_result(output_file=output_file)
+
+
+
+class TestHyperTuning(unittest.TestCase):
+    def test_GRU(self):
+        tune(algo="bayes")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- recbole/model/general_recommender/cvae.py
+++ recbole/model/general_recommender/cvae.py
@@ -13,6 +13,7 @@ MacridVAE
 ################################################
 Reference:
     Jianxin Ma et al. "Learning Disentangled Representations for Recommendation." in NeurIPS 2019.
+
 Reference code:
     https://jianxinma.github.io/disentangle-recsys.html
 """
@@ -21,21 +22,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from recbole.model.abstract_recommender import AutoEncoderMixin, GeneralRecommender
+from recbole.model.abstract_recommender import GeneralRecommender
 from recbole.model.init import xavier_normal_initialization
 from recbole.model.loss import EmbLoss
 from recbole.utils import InputType
 
 
-class MacridVAE(GeneralRecommender, AutoEncoderMixin):
+class CVAE(GeneralRecommender):
     r"""MacridVAE is an item-based collaborative filtering model that learns disentangled representations from user
     behavior and simultaneously ranks all items for each user.
+
     We implement the model following the original author.
     """
     input_type = InputType.PAIRWISE
 
     def __init__(self, config, dataset):
-        super(MacridVAE, self).__init__(config, dataset)
+        super(CVAE, self).__init__(config, dataset)
 
         self.layers = config["encoder_hidden_size"]
         self.embedding_size = config["embedding_size"]
@@ -49,23 +51,56 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
         self.std = config["std"]
 
         self.update = 0
-        self.build_histroy_items(dataset)
+
+        self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
+        pretrained_item_emb = dataset.get_preload_weight('iid')
+        self.l_embedding_size = pretrained_item_emb.shape[1]
+        self.label = nn.Embedding.from_pretrained(torch.from_numpy(pretrained_item_emb), freeze=False).type(torch.FloatTensor)
+        
+        self.k_embedding = nn.Embedding(self.kfac, self.embedding_size + self.l_embedding_size) ##
+    
+
+        self.history_item_id, self.history_item_value, _ = dataset.history_item_matrix()
+        self.history_item_id = self.history_item_id.to(self.device)
+        self.history_item_value = self.history_item_value.to(self.device)
         self.encode_layer_dims = (
-            [self.n_items] + self.layers + [self.embedding_size * 2]
+            [self.n_items] + self.layers + [(self.embedding_size + self.l_embedding_size) * 2]
         )
 
         self.encoder = self.mlp_layers(self.encode_layer_dims)
-
-        self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
-        self.k_embedding = nn.Embedding(self.kfac, self.embedding_size)
 
         self.l2_loss = EmbLoss()
         # parameters initialization
         self.apply(xavier_normal_initialization)
 
+    def get_rating_matrix(self, user):
+        r"""Get a batch of user's feature with the user's id and history interaction matrix.
+
+        Args:
+            user (torch.LongTensor): The input tensor that contains user's id, shape: [batch_size, ]
+
+        Returns:
+            torch.FloatTensor: The user's feature of a batch of user, shape: [batch_size, n_items]
+        """
+        # Following lines construct tensor of shape [B,n_items] using the tensor of shape [B,H]
+        col_indices = self.history_item_id[user].flatten()
+        row_indices = (
+            torch.arange(user.shape[0])
+            .to(self.device)
+            .repeat_interleave(self.history_item_id.shape[1], dim=0)
+        )
+        rating_matrix = (
+            torch.zeros(1).to(self.device).repeat(user.shape[0], self.n_items)
+        )
+        rating_matrix.index_put_(
+            (row_indices, col_indices), self.history_item_value[user].flatten()
+        )
+        return rating_matrix
+
     def mlp_layers(self, layer_dims):
         mlp_modules = []
         for i, (d_in, d_out) in enumerate(zip(layer_dims[:-1], layer_dims[1:])):
+            #self.logger.info(f'Len D_in {(d_in)} D_out{(d_out)}')
             mlp_modules.append(nn.Linear(d_in, d_out))
             if i != len(layer_dims[:-1]) - 1:
                 mlp_modules.append(nn.Tanh())
@@ -75,18 +110,23 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
         if self.training:
             std = torch.exp(0.5 * logvar)
             epsilon = torch.zeros_like(std).normal_(mean=0, std=self.std)
+            #self.logger.info(mu.shape, epsilon.shape, std.shape)
             return mu + epsilon * std
         else:
             return mu
 
     def forward(self, rating_matrix):
+
         cores = F.normalize(self.k_embedding.weight, dim=1)
-        items = F.normalize(self.item_embedding.weight, dim=1)
+        items = F.normalize(self.item_embedding.weight , dim=1)
+        label = F.normalize(self.label.weight, dim=1) ##
+
+        input_weights = torch.concat((items, label), dim=1) ##
 
         rating_matrix = F.normalize(rating_matrix)
         rating_matrix = F.dropout(rating_matrix, self.drop_out, training=self.training)
 
-        cates_logits = torch.matmul(items, cores.transpose(0, 1)) / self.tau
+        cates_logits = torch.matmul(input_weights, cores.transpose(0, 1)) / self.tau ##
 
         if self.nogb:
             cates = torch.softmax(cates_logits, dim=-1)
@@ -102,19 +142,33 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
             cates_k = cates[:, k].reshape(1, -1)
             # encoder
             x_k = rating_matrix * cates_k
+
+            #self.logger.info(f'X Shape {x_k.shape} Rating Matrix {rating_matrix.shape} Cates K {cates_k.shape} Labels {label.shape}')
+            
+            #x_k = torch.concat([x_k, self.label.weight.T])
+            #self.logger.info(f'X Shape {x_k.shape}')
+
             h = self.encoder(x_k)
-            mu = h[:, : self.embedding_size]
+            mu = h[:, : self.embedding_size + self.l_embedding_size]
             mu = F.normalize(mu, dim=1)
-            logvar = h[:, self.embedding_size :]
+
+            logvar = h[:, self.embedding_size + self.l_embedding_size :]
+
+            #self.logger.info(f'H Shape {h.shape}\tMu Shape {mu.shape}\tLogVar {logvar.shape}')
 
             mulist.append(mu)
             logvarlist.append(logvar)
 
             z = self.reparameterize(mu, logvar)
 
+            #self.logger.info('VAR SHAPES')
+
             # decoder
-            z_k = F.normalize(z, dim=1)
-            logits_k = torch.matmul(z_k, items.transpose(0, 1)) / self.tau
+            z_k = F.normalize(z, dim=1) 
+
+            #self.logger.info(f'Z_k: {z_k.shape} Input Weights: {input_weights.shape} Label: {label.shape}')
+
+            logits_k = torch.matmul(z_k, input_weights.transpose(0, 1)) / self.tau
             probs_k = torch.exp(logits_k)
             probs_k = probs_k * cates_k
             probs = probs_k if (probs is None) else (probs + probs_k)
@@ -124,6 +178,7 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
         return logits, mulist, logvarlist
 
     def calculate_loss(self, interaction):
+
         user = interaction[self.USER_ID]
 
         rating_matrix = self.get_rating_matrix(user)
@@ -135,6 +190,7 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
             anneal = self.anneal_cap
 
         z, mu, logvar = self.forward(rating_matrix)
+        self.logger.info(f'Loss Function: Z {len(z)}, logvar {len(logvar)}')
         kl_loss = None
         for i in range(self.kfac):
             kl_ = -0.5 * torch.mean(torch.sum(1 + logvar[i] - logvar[i].exp(), dim=1))
@@ -151,6 +207,7 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
     def reg_loss(self):
         r"""Calculate the L2 normalization loss of model parameters.
         Including embedding matrices and weight matrices of model.
+
         Returns:
             loss(torch.FloatTensor): The L2 Loss tensor. shape of [1,]
         """
@@ -164,6 +221,7 @@ class MacridVAE(GeneralRecommender, AutoEncoderMixin):
         return loss_1 + loss_2 + loss_3
 
     def predict(self, interaction):
+
         user = interaction[self.USER_ID]
         item = interaction[self.ITEM_ID]
 
