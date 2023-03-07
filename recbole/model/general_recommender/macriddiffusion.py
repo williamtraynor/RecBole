3812dfa0286e2a449079b8f8d9cf3384311b13b7
williamtraynor <2381137t@student.gla.ac.uk>
2023-03-07 11:01:16 +0000

    macrid revert with anneal (no k in diffusion)

--- recbole/model/general_recommender/macriddiffusion.py
+++ recbole/model/general_recommender/macriddiffusion.py
@@ -80,8 +80,8 @@ class MacridDiffusion(GeneralRecommender):
         self.encoder = self.mlp_layers(self.encode_layer_dims)
 
         self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
-        #pretrained_item_emb = dataset.get_preload_weight('iid')
-        #self.item_embedding = nn.Embedding.from_pretrained(torch.from_numpy(pretrained_item_emb), freeze=False).type(torch.FloatTensor)
+        pretrained_item_emb = dataset.get_preload_weight('iid')
+        self.mm_representation = nn.Embedding.from_pretrained(torch.from_numpy(pretrained_item_emb), freeze=False).type(torch.FloatTensor)
         
         
         self.k_embedding = nn.Embedding(self.kfac, self.embedding_size)
@@ -100,12 +100,12 @@ class MacridDiffusion(GeneralRecommender):
 
         self.apply(xavier_normal_initialization)
 
-    def forward_diffusion_sample(self, rating_matrix, t):
+    # Return forward diffusion sample that is equal for every k.
+    def forward_diffusion_sample_k(self, rating_matrix, t):
         """ 
         Takes an image and a timestep as input and 
         returns the noisy version of it
         """
-
         
         cores = F.normalize(self.k_embedding.weight, dim=1)
         items = F.normalize(self.item_embedding.weight, dim=1)
@@ -145,6 +145,64 @@ class MacridDiffusion(GeneralRecommender):
 
         return meanlist, varlist
 
+
+    # Return a forward diffusion sample for each user prototype, k.
+    def forward_diffusion_sample(self, X, t):
+        """ 
+        Takes an image and a timestep as input and 
+        returns the noisy version of it
+        """
+
+        #x_0 = self.encoder(rating_matrix) # x = Rating Matrix
+
+        noise = torch.randn_like(X)
+        sqrt_alphas_cumprod_t = self.get_index_from_list(self.sqrt_alphas_cumprod, t, X.shape)
+        sqrt_one_minus_alphas_cumprod_t = self.get_index_from_list(
+            self.sqrt_one_minus_alphas_cumprod, t, X.shape
+        )
+        # mean + variance
+        return sqrt_alphas_cumprod_t.to(self.device) * X.to(self.device) \
+        + sqrt_one_minus_alphas_cumprod_t.to(self.device) * noise.to(self.device), noise.to(self.device)
+        
+        '''
+        cores = F.normalize(self.k_embedding.weight, dim=1)
+        items = F.normalize(self.item_embedding.weight, dim=1)
+
+        rating_matrix = F.normalize(rating_matrix)
+        rating_matrix = F.dropout(rating_matrix, self.drop_out, training=self.training)
+
+        cates_logits = torch.matmul(items, cores.transpose(0, 1)) / self.tau
+
+        if self.nogb:
+            cates = torch.softmax(cates_logits, dim=-1)
+        else:
+            cates_sample = F.gumbel_softmax(cates_logits, tau=1, hard=False, dim=-1)
+            cates_mode = torch.softmax(cates_logits, dim=-1)
+            cates = self.training * cates_sample + (1 - self.training) * cates_mode
+
+        meanlist = []
+        varlist = []
+        for k in range(self.kfac):
+            cates_k = cates[:, k].reshape(1, -1)
+            # encoder
+            x_k = rating_matrix * cates_k
+
+            X = self.encoder(x_k)
+            
+            noise = torch.randn_like(X)
+            sqrt_alphas_cumprod_t = self.get_index_from_list(self.sqrt_alphas_cumprod, t, X.shape)
+            sqrt_one_minus_alphas_cumprod_t = self.get_index_from_list(
+                self.sqrt_one_minus_alphas_cumprod, t, X.shape
+            )
+            # mean + variance
+            mean, var =  sqrt_alphas_cumprod_t.to(self.device) * X.to(self.device) \
+            + sqrt_one_minus_alphas_cumprod_t.to(self.device) * noise.to(self.device), noise.to(self.device)
+
+            meanlist += mean,
+            varlist += var,
+
+        return meanlist, varlist'''
+
     def get_rating_matrix(self, user):
         r"""Get a batch of user's feature with the user's id and history interaction matrix.
 
@@ -175,7 +233,7 @@ class MacridDiffusion(GeneralRecommender):
         while considering the batch dimension.
         """
         batch_size = t.shape[0]
-        out = vals.gather(-1, t.cpu()-1)
+        out = vals.gather(-1, t.cpu())
         return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
     def mlp_layers(self, layer_dims):
@@ -246,6 +304,7 @@ class MacridDiffusion(GeneralRecommender):
             # encoder
             x_k = rating_matrix * cates_k
             h = self.encoder(x_k)
+
             # Diffusion takes place of commented out lines below from MultiVAE architecture.
             z, mu, logvar = self.diffusion(h, t)
 
@@ -267,9 +326,10 @@ class MacridDiffusion(GeneralRecommender):
     def calculate_loss(self, interaction):
         user = interaction[self.USER_ID]
         rating_matrix = self.get_rating_matrix(user)
+        h = F.normalize(rating_matrix)
 
         # t is uniform random variable from {1,2,...,n_steps}
-        t = torch.randint(1, self.n_steps+1, (rating_matrix.shape[0],), device=self.device).long()
+        t = torch.randint(0, self.n_steps, (rating_matrix.shape[0],), device=self.device).long()
 
         # anneal calcualtion - need to look into!
         self.update += 1
@@ -291,17 +351,20 @@ class MacridDiffusion(GeneralRecommender):
         ce_loss = -(F.log_softmax(z, 1) * rating_matrix).sum(1).mean()
 
         # Diffusion Loss
-        x_noisylist, noiselist = self.forward_diffusion_sample(rating_matrix, t)
-        diffusion_loss = 0
-        for i in range(self.kfac):
-            noise_pred, _, _ = self.diffusion(x_noisylist[i], t)
-            dl_ = F.mse_loss(noiselist[i], noise_pred)
-            diffusion_loss = dl_ if (diffusion_loss is None) else (diffusion_loss + dl_)
+        x_noisy, noise = self.forward_diffusion_sample(self.encoder(h), t)
+        ##x_noisylist, noiselist = self.forward_diffusion_sample(rating_matrix, t)
+        #diffusion_loss = 0
+        #for i in range(self.kfac):
+        noise_pred, _, _ = self.diffusion(x_noisy, t)
+        diffusion_loss = F.mse_loss(noise, noise_pred)
+            ##noise_pred, _, _ = self.diffusion(x_noisylist[i], t)
+            ##dl_ = F.mse_loss(noiselist[i], noise_pred)
+        #diffusion_loss = dl_ if (diffusion_loss is None) else (diffusion_loss + dl_)
         
         #if self.regs[0] != 0 or self.regs[1] != 0:
         #    return ce_loss + kl_loss * anneal + self.reg_loss()
 
-        return ce_loss + diffusion_loss #+ kl_loss * anneal 
+        return ce_loss + diffusion_loss * anneal #+ kl_loss * anneal 
 
     def reg_loss(self):
         r"""Calculate the L2 normalization loss of model parameters.
@@ -325,7 +388,7 @@ class MacridDiffusion(GeneralRecommender):
         h = self.get_rating_matrix(user)
 
         # t = T for evaluation
-        t = torch.full((h.shape[0],), self.n_steps, device=self.device).long()
+        t = torch.full((h.shape[0],), self.n_steps-1, device=self.device).long()
 
         scores, _, _, _ = self.forward(h, t)
 
@@ -337,7 +400,7 @@ class MacridDiffusion(GeneralRecommender):
         h = self.get_rating_matrix(user)
 
         # t = T for evaluation
-        t = torch.full((h.shape[0],), self.n_steps, device=self.device).long()
+        t = torch.full((h.shape[0],), self.n_steps-1, device=self.device).long()
 
         scores, _, _, _ = self.forward(h, t)
 
